88e1478b181bb8dae7d9ff4cabc230713a79ad9c
ARIEL RICARDO DIAZ <adiaz@perceptiongrp.com>
2019-05-03 00:39:59 -0300

    analytics

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const textEs = "Soy desarrollador web con Javascript, Angular, React, Node y .NE
 const textEn = "I'm web developer, I use Javascript, Angular, React, Node and .NET";
 
 const changeLanguage = () => {
-  const buttonLanguage = document.querySelector('.button-language');
   const contentText = document.querySelector('.description p');
   const value = buttonLanguage.innerText == "EN" ? "ES" : "EN";
+
   contentText.innerText = value == "ES" ? textEn : textEs;
-  buttonLanguage.innerText = value;
+  document.querySelector('.button-language').innerText = value;
+
 };
